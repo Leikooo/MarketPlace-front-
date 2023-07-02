e841487d0eb6b9ba3b0214ca7aea43bb4f9805c2
Никита <93408249+Leikooo@users.noreply.github.com>
2023-07-02 03:30:31 +0300

    newslettet ready + cards start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,29 @@
 
+import './homePage.scss'
+
+import PresentText from '@/components/PresentText'
+import Newsletter from '@/components/Newsletter'
+import NewsContentSlider from '@/components/NewsContentSlider'
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="Home">
+      <div className="HomeContent">
+        <div className="PresentText">
+          <PresentText />
+        </div>
+        <div className="InTheNews">
+          <div className="InTheNewsText">
+            <p>In the news</p>
+          </div>
+          <div className="NewsContentSlider">
+            <NewsContentSlider />
+          </div>
+        </div>
+        <div className="Newsletter">
+          <Newsletter />
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/PresentText.tsx
@@ -0,0 +1,14 @@
+export default function PresentText() {
+    return (
+      <div className="PresentText">
+        <div className="Info">
+          <p>Your digital needs</p>
+          <p>Our marketplace solutions</p>
+        </div>
+        <div className="UnderInfo">
+          <p>The fundamental protocol that allows anyone to create and exchange value,</p>
+          <p>powered by zero-knowledge technology.</p>
+        </div>
+      </div>
+    )
+  }
